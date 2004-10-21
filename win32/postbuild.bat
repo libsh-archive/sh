22f268faa1026a151283897cfdf67044522d0622
mjchase <mjchase@afdca40c-03d6-0310-8ede-e9f093b21075>
2004-10-21 15:58:18 +0000

    VC++ project for Sh
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/branches/directx@1600 afdca40c-03d6-0310-8ede-e9f093b21075

---
+++ win32/postbuild.bat
@@ -0,0 +1,8 @@
+copy ..\src\*.hpp ..\..\include\sh
+copy ..\util\*.hpp ..\..\include\sh
+copy Release\*.lib ..\..\lib
+copy Release\*.dll ..\..\lib
+copy Debug\*.lib ..\..\lib
+copy Debug\*.dll ..\..\lib
+copy Debug\*.pdb ..\..\lib
+
---
+++ win32/prebuild.bat
@@ -0,0 +1,4 @@
+cd ..\..
+IF NOT EXIST lib mkdir lib
+IF NOT EXIST include mkdir include
+IF NOT EXIST include\sh mkdir include\sh
