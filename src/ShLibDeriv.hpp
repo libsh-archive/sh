f97b8f2c4d315ea744e018e5a2b781f97304e888
b8chan <b8chan@afdca40c-03d6-0310-8ede-e9f093b21075>
2004-11-28 06:19:50 +0000

    See previous checkin. Part 1 - ShLibs
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/trunk@1710 afdca40c-03d6-0310-8ede-e9f093b21075

--- src/ShLibDeriv.hpp
+++ src/ShLibDeriv.hpp
@@ -42,28 +42,28 @@ namespace SH {
 
 /** Screen-space x derivatives
  */
-template<int N, ShValueType V>
-ShGeneric<N, V> dx(const ShGeneric<N, V>& var);
+template<int N, typename T>
+ShGeneric<N, T> dx(const ShGeneric<N, T>& var);
 
 /** Screen-space y derivatives
  */
-template<int N, ShValueType V>
-ShGeneric<N, V> dy(const ShGeneric<N, V>& var);
+template<int N, typename T>
+ShGeneric<N, T> dy(const ShGeneric<N, T>& var);
 
 /** Maximum value of absolute derivatives
  */
-template<int N, ShValueType V>
-ShGeneric<N, V> fwidth(const ShGeneric<N, V>& var);
+template<int N, typename T>
+ShGeneric<N, T> fwidth(const ShGeneric<N, T>& var);
 
 /** Pair of screen-space derivatives
  */
-template<ShValueType V>
-ShGeneric<2, V> gradient(const ShGeneric<1, V>& var);
+template<typename T>
+ShGeneric<2, T> gradient(const ShGeneric<1, T>& var);
 
 /** Jacobian matrix
  */
-template<int N, ShValueType V>
-ShMatrix<2, N, SH_TEMP, V> jacobian(const ShGeneric<N, V>& var);
+template<int N, typename T>
+ShMatrix<2, N, SH_TEMP, T> jacobian(const ShGeneric<N, T>& var);
 
 /*@}*/
 
