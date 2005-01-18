29c1644c767129cc7ce72685d7368e92c244c10f
francois <francois@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-01-18 16:56:18 +0000

    Uncomment all the tests, even the ones that fail.  These will need to be fixed or set to return 77 (so that automake ignores them).
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/trunk@1857 afdca40c-03d6-0310-8ede-e9f093b21075

--- test/regress/frac.cpp.py
+++ test/regress/frac.cpp.py
@@ -5,7 +5,7 @@ import shtest, sys, common
 test = shtest.StreamTest('frac', 1)
 test.add_call(shtest.Call(shtest.Call.call, 'frac', 1))
 
-#test.add_test(shtest.make_test([0, 0.9, 0.5, 0.1], [[-8.0, -8.9, -8.5, -8.1]], []))
+test.add_test(shtest.make_test([0, 0.1, 0.5, 0.9], [[-8.0, -8.9, -8.5, -8.1]], []))
 test.add_test(shtest.make_test([0, 0.5, 0, 0.5], [[-0.0, -0.5, 0.0, 0.5]], []))
 test.add_test(shtest.make_test([0, 0.9, 0.5, 0.1], [[8.0, 8.9, 8.5, 8.1]], []))
 
--- test/regress/mod.cpp.py
+++ test/regress/mod.cpp.py
@@ -8,18 +8,18 @@ def mod(p, q, types=[]):
 
 def insert_into(test):
     test.add_test(mod((3, 3, 3), (4, 3, 2), ['i', 'i', 'i']))
-    #test.add_test(mod((-3, -3, -3), (4, 3, 2), ['i', 'i', 'i']))
-    #test.add_test(mod((3, 3, 3), (-4, -3, -2), ['i', 'i', 'i']))
+    test.add_test(mod((-3, -3, -3), (4, 3, 2), ['i', 'i', 'i']))
+    test.add_test(mod((3, 3, 3), (-4, -3, -2), ['i', 'i', 'i']))
     test.add_test(mod((-3, -3, -3), (-4, -3, -2), ['i', 'i', 'i']))
     
     test.add_test(mod((3.0, 3.0, 3.0), (4.0, 3.0, 2.0), ['f', 'f', 'f']))
     test.add_test(mod((-3.0, -3.0, -3.0), (4.0, 3.0, 2.0), ['f', 'f', 'f']))
-    #test.add_test(mod((3.0, 3.0, 3.0), (-4.0, -3.0, -2.0), ['f', 'f', 'f']))
+    test.add_test(mod((3.0, 3.0, 3.0), (-4.0, -3.0, -2.0), ['f', 'f', 'f']))
     test.add_test(mod((-3.0, -3.0, -3.0), (-4.0, -3.0, -2.0), ['f', 'f', 'f']))
 
     test.add_test(mod((3.1, 3.5, 3.9), (4.0, 3.0, 2.0), ['f', 'f', 'f']))
-    #test.add_test(mod((-3.1, -3.5, -3.9), (4.0, 3.0, 2.0), ['f', 'f', 'f']))
-    #test.add_test(mod((3.1, 3.5, 3.9), (-4.0, -3.0, -2.0), ['f', 'f', 'f']))
+    test.add_test(mod((-3.1, -3.5, -3.9), (4.0, 3.0, 2.0), ['f', 'f', 'f']))
+    test.add_test(mod((3.1, 3.5, 3.9), (-4.0, -3.0, -2.0), ['f', 'f', 'f']))
     test.add_test(mod((-3.1, -3.5, -3.9), (-4.0, -3.0, -2.0), ['f', 'f', 'f']))
 
 test = shtest.StreamTest('mod', 2)
