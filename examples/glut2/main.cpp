84c36ff0b674915c09978ffdca0e893e45192d3a
francois <francois@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-02-18 21:53:16 +0000

    Adding a barebone glut example
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/branches/glsl@2022 afdca40c-03d6-0310-8ede-e9f093b21075

--- examples/glut2/main.cpp
+++ examples/glut2/main.cpp
@@ -2,20 +2,11 @@
 #include <GL/glut.h>
 #include <GL/glext.h>
 #include <GL/glu.h>
-#include "Camera.hpp"
 #include <iostream>
 
 using namespace SH;
-using namespace std;
 
-//ShMatrix4x4f mv, mvd;
-//ShPoint3f lightPos;
-Camera camera;
 ShProgram vsh, fsh;
-
-// Glut data
-int buttons[5] = {GLUT_UP, GLUT_UP, GLUT_UP, GLUT_UP, GLUT_UP};
-int cur_x, cur_y;
 
 void initShaders()
 {
@@ -45,80 +36,23 @@ void initShaders()
     normal = mv | normal; // Project normal
   } SH_END;
 
-  //ShColor3f SH_DECL(diffusecolor) = ShColor3f(0.5, 0.7, 0.9);
-  
   fsh = SH_BEGIN_FRAGMENT_PROGRAM {
-    ShInputPosition4f pos;
     ShInputNormal3f normal;
     ShInputVector3f lightv;
-
-    ShOutputColor3f color;
+    ShOutputColor4f color;
 
     normal = normalize(normal);
     lightv = normalize(lightv);
     
-    //color = (normal | lightv) * diffusecolor;
-    color = (normal | lightv) * ShColor3f(0.5, 0.7, 0.9);
+    color = (normal | lightv) * ShColor4f(0.5, 0.7, 0.9, 1.0);
   } SH_END;
 }
 
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  glFrontFace(GL_CW);
   glutSolidTeapot(2.5);
-  glFrontFace(GL_CCW);
-  
   glutSwapBuffers();
-}
-
-void setupView()
-{
-  //mv = camera.shModelView();
-  //mvd = camera.shModelViewProjection(ShMatrix4x4f());
-}
-
-void reshape(int width, int height)
-{
-  glViewport(0, 0, width, height);
-  setupView();
-}
-
-void motion(int x, int y)
-{
-  const double factor = 20.0;
-  bool changed = false;
-  
-  if (buttons[GLUT_LEFT_BUTTON] == GLUT_DOWN) {
-    camera.orbit(cur_x, cur_y, x, y,
-                 glutGet(GLUT_WINDOW_WIDTH),
-                 glutGet(GLUT_WINDOW_HEIGHT));
-    changed = true;
-  }
-  if (buttons[GLUT_MIDDLE_BUTTON] == GLUT_DOWN) {
-    camera.move(0, 0, (y - cur_y)/factor);
-    changed = true;
-  }
-  if (buttons[GLUT_RIGHT_BUTTON] == GLUT_DOWN) {
-    camera.move((x - cur_x)/factor, (cur_y - y)/factor, 0);
-    changed = true;
-  }
-
-  if (changed) {
-    setupView();
-    glutPostRedisplay();
-  }
-
-  cur_x = x;
-  cur_y = y;
-}
-
-void mouse(int button, int state, int x, int y)
-{
-  buttons[button] = state;
-  cur_x = x;
-  cur_y = y;
 }
 
 int main(int argc, char** argv)
@@ -128,32 +62,21 @@ int main(int argc, char** argv)
   glutInitWindowSize(512, 512);
   glutCreateWindow("Sh Example");
   glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-  glutMouseFunc(mouse);
-  glutMotionFunc(motion);
   
   shSetBackend("glsl");
 
-  initShaders();
-  
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_VERTEX_PROGRAM_ARB);
   glEnable(GL_FRAGMENT_PROGRAM_ARB);
   glClearColor(0.0, 0.0, 0.0, 1.0);
-  setupView();
 
-  // Place the camera at its initial position
-  camera.move(0.0, 0.0, -15.0);
-
-  // Set up the light position
-  //lightPos = ShPoint3f(5.0, 5.0, 5.0);
-  
   initShaders();
 
   shBind(vsh);
   shBind(fsh);
 
-#if 0
+#if 1
+  using namespace std;
   cout << "Vertex Unit:" << endl;
   vsh.node()->code()->print(cout);
   cout << "--" << endl;
