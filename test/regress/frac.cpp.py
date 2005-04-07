d5d52230e1def21b51d56e72f97fb91f58f7d3bd
francois <francois@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-04-07 18:12:14 +0000

    Added tests in immediate mode.
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/trunk@2280 afdca40c-03d6-0310-8ede-e9f093b21075

--- test/regress/frac.cpp.py
+++ test/regress/frac.cpp.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
 
 import shtest, sys, common
+from common import *
+from math import *
 
+def frac_test(p, types=[]):
+    if is_array(p):
+        result = [a - floor(a) for a in p]
+    else:
+        result = [a - floor(a)]
+    return shtest.make_test(result, [p], types)
+
+def insert_into(test):
+    test.add_test(frac_test((-8.0, -8.9, -8.5, -8.1)))
+    test.add_test(frac_test((-0.0, -0.5, 0.0, 0.5)))
+    test.add_test(frac_test((8.0, 8.9, 8.5, 8.1)))
+
+# Test frac in stream programs
 test = shtest.StreamTest('frac', 1)
 test.add_call(shtest.Call(shtest.Call.call, 'frac', 1))
+insert_into(test)
+test.output_header(sys.stdout)
+test.output(sys.stdout, False)
 
-test.add_test(shtest.make_test([0, 0.1, 0.5, 0.9], [[-8.0, -8.9, -8.5, -8.1]], []))
-test.add_test(shtest.make_test([0, 0.5, 0, 0.5], [[-0.0, -0.5, 0.0, 0.5]], []))
-test.add_test(shtest.make_test([0, 0.9, 0.5, 0.1], [[8.0, 8.9, 8.5, 8.1]], []))
+# Test frac in immediate mode
+test = shtest.ImmediateTest('frac_im', 1)
+test.add_call(shtest.Call(shtest.Call.call, 'frac', 1))
+insert_into(test)
+test.output(sys.stdout, False)
 
-test.output(sys.stdout)
+test.output_footer(sys.stdout)
