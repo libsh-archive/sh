f2cbb0fcab1e3c44ba95408900d534c77a54ee72
sjdutoit <sjdutoit@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-06-02 22:40:19 +0000

    Update the postbuild scripts for _debug.dll instead of "d.dll" in the debug compiles.
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/trunk@2458 afdca40c-03d6-0310-8ede-e9f093b21075

--- win32/postbuild_glsl.bat
+++ win32/postbuild_glsl.bat
@@ -1,5 +1,5 @@
-IF EXIST Debug\libshglsld.pdb copy Debug\libshglsld.pdb ..\..\install
-IF EXIST Debug\libshglsld.dll copy Debug\libshglsld.dll ..\..\install
+IF EXIST Debug\libshglsl_debug.pdb copy Debug\libshglsl_debug.pdb ..\..\install
+IF EXIST Debug\libshglsl_debug.dll copy Debug\libshglsl_debug.dll ..\..\install
 
 IF EXIST Release\libshglsl.pdb copy Release\libshglsl.pdb ..\..\install
 IF EXIST Release\libshglsl.dll copy Release\libshglsl.dll ..\..\install
--- win32/postbuild_sh.bat
+++ win32/postbuild_sh.bat
@@ -1,8 +1,8 @@
 copy ..\src\*.hpp ..\..\install\include\sh
 
-IF EXIST Debug\libshd.dll copy Debug\libshd.dll ..\..\install
-IF EXIST Debug\libshd.pdb copy Debug\libshd.pdb ..\..\install
-IF EXIST Debug\libshd.lib copy Debug\libshd.lib ..\..\install\lib
+IF EXIST Debug\libsh_debug.dll copy Debug\libsh_debug.dll ..\..\install
+IF EXIST Debug\libsh_debug.pdb copy Debug\libsh_debug.pdb ..\..\install
+IF EXIST Debug\libsh_debug.lib copy Debug\libsh_debug.lib ..\..\install\lib
 
 IF EXIST Release\libsh.dll copy Release\libsh.dll ..\..\install
 IF EXIST Release\libsh.pdb copy Release\libsh.pdb ..\..\install
