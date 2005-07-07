63a13bfc3157b48aa60239188b5e94570bf1431b
wdonnell <wdonnell@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-07-07 20:48:12 +0000

    Floating point constants are now written to ARB shaders at higher precision.
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/trunk@2702 afdca40c-03d6-0310-8ede-e9f093b21075

--- backends/gl/ArbReg.cpp
+++ backends/gl/ArbReg.cpp
@@ -101,7 +101,8 @@ std::ostream& ArbReg::printDecl(std::ostream& out) const
     for (int i = 0; i < 4; i++) {
       if (i) out << ", ";
       std::streamsize prec = out.precision();
-      out.precision(std::numeric_limits<float>::digits10);
+//      out.precision(std::numeric_limits<float>::digits10);
+      out.precision(20);
       out << binding.values[i];
       out.precision(prec);
     }
