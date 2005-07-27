71fc43aa5bc7ba5c47a173a0edee814987dfc4e7
atlaurit <atlaurit@afdca40c-03d6-0310-8ede-e9f093b21075>
2005-07-27 14:21:37 +0000

    Moved Lua5.1+Mutate operators into the Sh trunk.
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/trunk@2822 afdca40c-03d6-0310-8ede-e9f093b21075

---
+++ lua/win32/lua/postbuild_lua.bat
@@ -0,0 +1,1 @@
+IF EXIST Release\lua.exe copy Release\lua.exe ..\..\..\install\lua\bin
---
+++ lua/win32/lua/prebuild_lua.bat
@@ -0,0 +1,6 @@
+cd ..\..\..
+IF NOT EXIST install mkdir install
+cd install
+IF NOT EXIST lua     mkdir lua
+cd lua
+IF NOT EXIST bin     mkdir bin
