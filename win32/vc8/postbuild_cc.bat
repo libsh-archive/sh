6a2027a412819b434978ebbe0f2bc43d2d756159
atlaurit <atlaurit@afdca40c-03d6-0310-8ede-e9f093b21075>
2006-01-05 16:45:22 +0000

    Added VC8 project files, solution and associated scripts.
    These work in the VC8 Express (with the Platform SDK), and should also work in the retail versions.
    
    We should probably also move the VC7 files to a "vc7\" directory to be consistent.
    
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/trunk@3362 afdca40c-03d6-0310-8ede-e9f093b21075

---
+++ win32/vc8/postbuild_cc.bat
@@ -0,0 +1,5 @@
+IF EXIST "Debug\libshcc_debug.dll" xcopy /D /Q /Y /K "Debug\libshcc_debug.dll" "..\..\..\install"
+IF EXIST "Debug\libshcc_debug.pdb" xcopy /D /Q /Y /K "Debug\libshcc_debug.pdb" "..\..\..\install"
+
+IF EXIST "Release\libshcc.dll" xcopy /D /Q /Y /K "Release\libshcc.dll" "..\..\..\install"
+IF EXIST "Release\libshcc.pdb" xcopy /D /Q /Y /K "Release\libshcc.pdb" "..\..\..\install"
---
+++ win32/vc8/postbuild_sh.bat
@@ -0,0 +1,10 @@
+xcopy /D /Q /Y /K "..\..\src\*.hpp" "..\..\..\install\include\sh"
+
+IF EXIST "Debug\libsh_debug.dll" xcopy /D /Q /Y /K "Debug\libsh_debug.dll" "..\..\..\install"
+IF EXIST "Debug\libsh_debug.pdb" xcopy /D /Q /Y /K "Debug\libsh_debug.pdb" "..\..\..\install"
+IF EXIST "Debug\libsh_debug.lib" xcopy /D /Q /Y /K "Debug\libsh_debug.lib" "..\..\..\install\lib"
+
+IF EXIST "Release\libsh.dll" xcopy /D /Q /Y /K "Release\libsh.dll" "..\..\..\install"
+IF EXIST "Release\libsh.pdb" xcopy /D /Q /Y /K "Release\libsh.pdb" "..\..\..\install"
+IF EXIST "Release\libsh.lib" xcopy /D /Q /Y /K "Release\libsh.lib" "..\..\..\install\lib"
+
