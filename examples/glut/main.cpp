1911ca7ea9a3c02a0ba5be1fe8647e6b0ecdebd6
atlaurit <atlaurit@afdca40c-03d6-0310-8ede-e9f093b21075>
2006-01-28 20:45:37 +0000

    Fixed a type in the GlutExample/Debug mode. Added some basic exception handling if shader compilation fails in the Glut example.
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/trunk@3440 afdca40c-03d6-0310-8ede-e9f093b21075

--- examples/glut/main.cpp
+++ examples/glut/main.cpp
@@ -74,8 +74,15 @@ void initShaders()
 
     normal = normalize(normal);
     lightv = normalize(lightv);
-    
-    color = (normal | lightv) * diffusecolor;
+
+    ShAttrib1f dp = normal | lightv;
+    ShAttrib1f compare = dp < 0.1f;
+        
+    SH_IF(compare) {
+      color = ShColor3f(1,0,1);
+    } SH_ELSE {
+      color = dp * diffusecolor;
+    } SH_ENDIF;
   } SH_END;
 
   shaders = new ShProgramSet(vsh, fsh);
@@ -253,11 +260,19 @@ int main(int argc, char** argv)
   lightPos = ShPoint3f(5.0, 5.0, 5.0);
   diffusecolor = ShColor3f(0.5, 0.1, 0.2);
 
-  initShaders();
+  try {
+    initShaders();
+    shBind(*shaders);
+  }
+  catch (const std::exception &e) {
+    cerr << e.what() << endl;
+#ifdef _WIN32
+    system("pause");
+#endif
+    return 1;
+  }
 
-  shBind(*shaders);
-
-#if 0
+#if 1
   cout << "Vertex Unit:" << endl;
   vsh.node()->code()->print(cout);
   cout << "--" << endl;
