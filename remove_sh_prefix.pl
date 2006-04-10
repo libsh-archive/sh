c7e589149d955ad1b2d290b28bca4df04be7e55e
francois <francois@afdca40c-03d6-0310-8ede-e9f093b21075>
2006-04-10 21:17:55 +0000

    Add the script used to remove the Sh prefix inside source files
    git-svn-id: svn://svn.code.sf.net/p/libsh/code/sh/branches/prefix_removed@3660 afdca40c-03d6-0310-8ede-e9f093b21075

---
+++ remove_sh_prefix.pl
@@ -0,0 +1,99 @@
+#!/usr/bin/perl
+
+use strict;
+
+use File::Copy;
+use File::Find;
+use File::Temp;
+
+# Directories which contain files to be "licensified"
+my @SRC_DIRS = ('backends', 'src', 'test', 'examples');
+
+my %special_macros = (
+    "BEGIN"    => "SH_BEGIN",
+    "END"      => "SH_END",
+    "IF"       => "SH_IF",
+    "ELSE"     => "SH_ELSE",
+    "ELSEIF"   => "SH_ELSEIF",
+    "FOR"      => "SH_FOR",
+    "ENDFOR"   => "SH_ENDFOR",
+    "DO"       => "SH_DO",
+    "UNTIL"    => "SH_UNTIL",
+    "WHILE"    => "SH_WHILE",
+    "ENDWHILE" => "SH_ENDWHILE",
+    "WHEN"     => "SH_WHEN",
+    "BREAK"    => "SH_BREAK",
+    "CONTINUE" => "SH_CONTINUE",
+    "RETURN"   => "SH_RETURN",
+);
+
+my %reserved_words = (
+    "if"       => "internal_if",
+    "else"     => "internal_else",
+    "break"    => "internal_break",
+    "continue" => "internal_continue",
+    "while"    => "internal_while",
+    "for"      => "internal_for",
+    "do"       => "internal_do",
+);
+
+sub substituted_macro
+{
+    $_ = shift;
+    $_ = $special_macros{$_} if exists $special_macros{$_};
+    return $_;
+}
+
+sub to_lower
+{
+    $_ = shift;
+    $_ =~ tr/[A-Z]/[a-z]/;
+    $_ = $reserved_words{$_} if exists $reserved_words{$_};
+    return $_;
+}
+
+sub process_file
+{
+    my $filename = shift;
+    print "Processing $filename... ";
+    open INPUT, "$filename" or die "Cannot open $filename as input.";
+
+    my @lines;
+    while (<INPUT>) {
+        # Header guards
+        s/ SH([A-Z]+)_HPP/ \1_HPP/g;
+        
+        # Macros/enums
+        s/(^|[^a-zA-Z])SH_?([A-Z]+)/$1 . &substituted_macro($2)/eg;
+
+        # Class names
+        s/([^a-zA-Z]?)Sh([A-Z])/\1\2/g;
+
+        # Function names (e.g. nibbles)
+        s/(^|[^a-zA-Z])sh([A-Z]+[a-z]+)/$1 . &to_lower($2)/eg;
+        push @lines, $_;
+    }
+    close INPUT;
+
+    open OUTPUT, ">$filename" or die "Cannot open $filename as output.";
+    print OUTPUT foreach @lines;
+    close OUTPUT;
+    print "done.\n";
+}
+
+sub process_standard_files
+{
+    # Find all .hpp and .cpp files in the standard directories
+    File::Find::find sub { /^.*\.[ch]pp\z/s && process_file($_) }, @SRC_DIRS;
+}
+
+sub main
+{
+    if (@ARGV > 0) {
+        process_file($ARGV[0]);
+    } else {
+        &process_standard_files;
+    }
+}
+
+&main;
